e6d8b33382af5c3fadc8900b512275d25e984e74
nac0303 <nathan_cordeiro03@hotmail.com>
2022-04-12 11:14:35 -0300

    Dia 1
    Dia 1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.VertLine1 = new System.Windows.Forms.PictureBox();
+            this.VertLine2 = new System.Windows.Forms.PictureBox();
+            this.HorLine1 = new System.Windows.Forms.PictureBox();
+            this.HorLine2 = new System.Windows.Forms.PictureBox();
+            this.Square3 = new System.Windows.Forms.PictureBox();
+            this.Square6 = new System.Windows.Forms.PictureBox();
+            this.Square5 = new System.Windows.Forms.PictureBox();
+            this.Square9 = new System.Windows.Forms.PictureBox();
+            this.Square8 = new System.Windows.Forms.PictureBox();
+            this.Square7 = new System.Windows.Forms.PictureBox();
+            this.Square4 = new System.Windows.Forms.PictureBox();
+            this.Square2 = new System.Windows.Forms.PictureBox();
+            this.Square1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.VertLine1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VertLine2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HorLine1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HorLine2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VertLine1
+            // 
+            this.VertLine1.BackColor = System.Drawing.Color.White;
+            this.VertLine1.Location = new System.Drawing.Point(439, 75);
+            this.VertLine1.Name = "VertLine1";
+            this.VertLine1.Size = new System.Drawing.Size(7, 480);
+            this.VertLine1.TabIndex = 2;
+            this.VertLine1.TabStop = false;
+            // 
+            // VertLine2
+            // 
+            this.VertLine2.BackColor = System.Drawing.Color.White;
+            this.VertLine2.Location = new System.Drawing.Point(606, 75);
+            this.VertLine2.Name = "VertLine2";
+            this.VertLine2.Size = new System.Drawing.Size(7, 480);
+            this.VertLine2.TabIndex = 3;
+            this.VertLine2.TabStop = false;
+            // 
+            // HorLine1
+            // 
+            this.HorLine1.BackColor = System.Drawing.Color.White;
+            this.HorLine1.Location = new System.Drawing.Point(285, 233);
+            this.HorLine1.Name = "HorLine1";
+            this.HorLine1.Size = new System.Drawing.Size(480, 7);
+            this.HorLine1.TabIndex = 0;
+            this.HorLine1.TabStop = false;
+            // 
+            // HorLine2
+            // 
+            this.HorLine2.BackColor = System.Drawing.Color.White;
+            this.HorLine2.Location = new System.Drawing.Point(285, 401);
+            this.HorLine2.Name = "HorLine2";
+            this.HorLine2.Size = new System.Drawing.Size(480, 7);
+            this.HorLine2.TabIndex = 1;
+            this.HorLine2.TabStop = false;
+            // 
+            // Square3
+            // 
+            this.Square3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Square3.Location = new System.Drawing.Point(619, 75);
+            this.Square3.Name = "Square3";
+            this.Square3.Size = new System.Drawing.Size(148, 152);
+            this.Square3.TabIndex = 6;
+            this.Square3.TabStop = false;
+            // 
+            // Square6
+            // 
+            this.Square6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Square6.Location = new System.Drawing.Point(617, 246);
+            this.Square6.Name = "Square6";
+            this.Square6.Size = new System.Drawing.Size(148, 152);
+            this.Square6.TabIndex = 7;
+            this.Square6.TabStop = false;
+            // 
+            // Square5
+            // 
+            this.Square5.BackColor = System.Drawing.Color.White;
+            this.Square5.Location = new System.Drawing.Point(452, 246);
+            this.Square5.Name = "Square5";
+            this.Square5.Size = new System.Drawing.Size(148, 152);
+            this.Square5.TabIndex = 8;
+            this.Square5.TabStop = false;
+            // 
+            // Square9
+            // 
+            this.Square9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Square9.Location = new System.Drawing.Point(619, 414);
+            this.Square9.Name = "Square9";
+            this.Square9.Size = new System.Drawing.Size(148, 152);
+            this.Square9.TabIndex = 12;
+            this.Square9.TabStop = false;
+            // 
+            // Square8
+            // 
+            this.Square8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Square8.Location = new System.Drawing.Point(452, 414);
+            this.Square8.Name = "Square8";
+            this.Square8.Size = new System.Drawing.Size(148, 152);
+            this.Square8.TabIndex = 11;
+            this.Square8.TabStop = false;
+            // 
+            // Square7
+            // 
+            this.Square7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Square7.Location = new System.Drawing.Point(285, 414);
+            this.Square7.Name = "Square7";
+            this.Square7.Size = new System.Drawing.Size(148, 152);
+            this.Square7.TabIndex = 10;
+            this.Square7.TabStop = false;
+            // 
+            // Square4
+            // 
+            this.Square4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Square4.Location = new System.Drawing.Point(285, 246);
+            this.Square4.Name = "Square4";
+            this.Square4.Size = new System.Drawing.Size(148, 152);
+            this.Square4.TabIndex = 9;
+            this.Square4.TabStop = false;
+            // 
+            // Square2
+            // 
+            this.Square2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Square2.Location = new System.Drawing.Point(452, 75);
+            this.Square2.Name = "Square2";
+            this.Square2.Size = new System.Drawing.Size(148, 152);
+            this.Square2.TabIndex = 5;
+            this.Square2.TabStop = false;
+            // 
+            // Square1
+            // 
+            this.Square1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Square1.Location = new System.Drawing.Point(285, 75);
+            this.Square1.Name = "Square1";
+            this.Square1.Size = new System.Drawing.Size(148, 152);
+            this.Square1.TabIndex = 4;
+            this.Square1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1074, 700);
+            this.Controls.Add(this.Square9);
+            this.Controls.Add(this.Square8);
+            this.Controls.Add(this.Square7);
+            this.Controls.Add(this.Square4);
+            this.Controls.Add(this.Square5);
+            this.Controls.Add(this.Square6);
+            this.Controls.Add(this.Square3);
+            this.Controls.Add(this.Square2);
+            this.Controls.Add(this.Square1);
+            this.Controls.Add(this.VertLine2);
+            this.Controls.Add(this.VertLine1);
+            this.Controls.Add(this.HorLine2);
+            this.Controls.Add(this.HorLine1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.VertLine1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VertLine2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HorLine1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HorLine2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Square1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox VertLine1;
+        private System.Windows.Forms.PictureBox VertLine2;
+        private System.Windows.Forms.PictureBox HorLine1;
+        private System.Windows.Forms.PictureBox HorLine2;
+        private System.Windows.Forms.PictureBox Square3;
+        private System.Windows.Forms.PictureBox Square6;
+        private System.Windows.Forms.PictureBox Square5;
+        private System.Windows.Forms.PictureBox Square9;
+        private System.Windows.Forms.PictureBox Square8;
+        private System.Windows.Forms.PictureBox Square7;
+        private System.Windows.Forms.PictureBox Square4;
+        private System.Windows.Forms.PictureBox Square2;
+        private System.Windows.Forms.PictureBox Square1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Center();
+        }
+
+        public void Center()
+        {
+            var squaresize = this.Height / 5;
+
+            VertLine1.Height = squaresize*3;
+            VertLine1.Left = (this.Width / 2) - (VertLine1.Width*2) - squaresize/2;
+            VertLine1.Top = squaresize;
+            VertLine2.Height = squaresize*3;
+            VertLine2.Left = (this.Width /2) - (VertLine1.Width * 2) + squaresize/2;
+            VertLine2.Top = squaresize;
+
+            HorLine1.Width = squaresize*3;
+            HorLine1.Left = (this.Width/2) - (VertLine1.Width * 2) - (HorLine1.Width / 2) ;
+            HorLine1.Top = (this.Height/2) - squaresize/2;
+            HorLine2.Width = squaresize * 3;
+            HorLine2.Left = (this.Width / 2) - (VertLine1.Width * 2) - (HorLine1.Width/2);
+            HorLine2.Top = (this.Height / 2) + squaresize/2;
+
+
+            Square1.Width = squaresize - VertLine1.Width * 3;
+            Square1.Height = squaresize - 14;
+            Square1.Left = (this.Width / 2) - (squaresize+ squaresize/2);
+            Square1.Top = squaresize + HorLine1.Height;
+
+            Square2.Width = squaresize - VertLine1.Width * 3;
+            Square2.Height = squaresize - 14;
+            Square2.Left = (this.Width / 2) - (squaresize / 2);
+            Square2.Top = squaresize + HorLine1.Height;
+
+            Square3.Width = squaresize - VertLine1.Width * 3;
+            Square3.Height = squaresize - 14;
+            Square3.Left = (this.Width / 2) + (squaresize/2);
+            Square3.Top = squaresize + HorLine1.Height;
+
+            Square4.Width = squaresize - VertLine1.Width * 3;
+            Square4.Height = squaresize - 14;
+            Square4.Left = (this.Width / 2) - (squaresize / 2);
+            Square4.Top = squaresize + HorLine1.Height;
+
+            Square5.Width = squaresize - VertLine1.Width * 3;
+            Square5.Height = squaresize - 14;
+            Square5.Left = (this.Width / 2) - (squaresize / 2);
+            Square5.Top = squaresize + HorLine1.Height;
+
+            Square6.Width = squaresize - VertLine1.Width*3;
+            Square6.Height = squaresize - 14;
+            Square6.Left = (this.Width / 2) - (squaresize / 2);
+            Square6.Top = squaresize + HorLine1.Height;
+
+            Square7.Width = squaresize - VertLine1.Width * 3;
+            Square7.Height = squaresize - 14;
+            Square7.Left = (this.Width / 2) - (squaresize / 2);
+            Square7.Top = squaresize + HorLine1.Height;
+
+            Square8.Width = squaresize - VertLine1.Width * 3;
+            Square8.Height = squaresize - 14;
+            Square8.Left = (this.Width / 2) - (squaresize / 2);
+            Square8.Top = squaresize + HorLine1.Height;
+
+            Square9.Width = squaresize - VertLine1.Width * 3;
+            Square9.Height = squaresize - 14;
+            Square9.Left = (this.Width / 2) - (squaresize / 2);
+            Square9.Top = squaresize + HorLine1.Height;
+
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Center();
+        }
+    }
+
+    
+}
